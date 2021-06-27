a28310da2c6354bd182a9ebb0ba9b2b04c4ab94c
Kavita Maurya <kavita.maurya@capgemini.com>
2021-06-27 21:57:29 +0530

    Folder structure created using cookie cutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='MLOPS implementation over cloud(Microsoft Azure, AWS, GCP)',
+    author='Inueron Internship ',
+    license='MIT',
+)
